2d53a577a6002f30a06ae8f15e94e72f10f02b5d
evargm <rodrigo2mauricio@hotmail.com>
2024-07-27 20:26:18 -0300

    turned to installable binary #9

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 print("Executing entrypoint for dundie...")
+||||||| parent of f1b8c17 (turned to installable binary #9)
+=======
+def main():
+    print("Executing entrypoint for dundie...")
+>>>>>>> f1b8c17 (turned to installable binary #9)
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from setuptools import setup, find_packages
 
 setup(
@@ -12,3 +13,25 @@ setup(
     packages=["dundie"],# geralmente todas as pastas de módulos no seu diretório
 
 )
+||||||| parent of f1b8c17 (turned to installable binary #9)
+=======
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",# será o nome que chamaremos com o pip ex: pip install dundie
+    version="0.1.0",# sistema de versionamento
+    description="Reward point system for dunder mifflin",
+    author="EvaRgm",# autor do projeto vulgo eu
+# se eu tiver pacotes em subpastas é aconcelhado utilizar o find_packages, que
+# irá procurar os pacotes/módulos em todos os diretórios inclusive o atual.
+# As pastas que não tiver um __init__.py serão ignoradas.
+  # packages=["dundie"],# geralmente todas as pastas de módulos no seu diretório
+    packages=find_packages(),
+    entry_points={
+        "console_scripts":[
+            "dundie = dundie.__main__:main"
+        ]
+    }
+    
+)
+>>>>>>> f1b8c17 (turned to installable binary #9)
